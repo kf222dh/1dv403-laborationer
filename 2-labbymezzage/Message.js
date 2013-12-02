5496cc693dba69c8b179d380507a491985f5b709
Kevin Fredriksson <kf222dh@student.lnu.se>
2013-12-02 16:06:24 -0500

    Mall

--- 2-labbymezzage/Message.js
+++ 2-labbymezzage/Message.js
@@ -1,1 +1,39 @@
 "use strict";
+
+function Message(message, date)
+{
+	this.getText = function()
+	{
+		return message;
+	}
+	
+	this.setText = function(_text)
+	{
+		message = text;
+	}
+	
+	this.getDate = function()
+	{
+	    
+	}
+	
+	this.setDate = function(_date)
+	{
+	    
+	}
+
+}
+	
+Message.prototype.toString = function() {
+	return this.getText()+" ("this.getDate()+")";
+}
+
+Message.prototype.getHTMLText = function()
+{
+    
+}
+
+Message.prototype.getDateText = function()
+{
+    
+}
