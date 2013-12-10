b9283bccfaa63c477562514f453f20e2eb494215
Kevin Fredriksson <kf222dh@student.lnu.se>
2013-12-10 17:54:43 -0500

    Steg 4 och 5

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -1,14 +1,19 @@
 "use strict";
 
-function MemoryBoard(memoryID)
+function MemoryBoard(memoryID, rows, cols)
 {
+    randomArray = [];
+    
     this.init = function()
     {
-        
+        this.randomArray = RandomGenerator.getPictureArray(rows, cols);
     };
+    
 }
 window.onload = function()
 {
+    
     new MemoryBoard("Board1").init();
     new MemoryBoard("Board2").init();
+    
 }
