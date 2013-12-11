0bee9201733bcee7f497bafe389cdea853b5a3c4
Kevin Fredriksson <kf222dh@student.lnu.se>
2013-12-11 18:29:08 -0500

    Hittade flera fel. Omformaterade till det. Klar

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -1,248 +1,249 @@
 (function()
-{
-"use strict";
 
-function MemoryBoard(memoryID, rows, cols)
 {
-    this.randomArray = [];
-    
-    this.init = function()
+    "use strict";
+
+    function MemoryBoard(memoryID, rows, cols)
     {
-        
-        this.randomArray = RandomGenerator.getPictureArray(rows, cols);
-        
-        var i = 0;
-        var x = 0;
-        var picCounter = 0;
-        
-        var lastAlternative = null;
-        var picUp = 0;
-        var This = this;
 
-        var table = document.createElement("table");
-        var div = document.getElementById(memoryID);
-        var tr = document.createElement("tr");
-        var img = document.createElement("img");
-        var anchor = document.createElement("a");
-        var td = document.createElement("td");
-        var pressDiv = document.createElement("div");
-        var p = document.createElement("p");
-        
-        var countGuesses = 0;
-        var duoFounded = 0;
-        
-        var againButton = document.createElement("button");
-        var clickEvent;
+        this.randomArray = [];
 
-        pressDiv.setAttribute("id", memoryID + "info");
-        
-        pressDiv.setAttribute("id", memoryID + "Info");
-        
-        pressDiv.setAttribute("class", "gameBoardinfo");
-
-        p = document.createElement("p");
-
-        pressDiv.appendChild(p);
-        
-        againButton.setAttribute("type", "button");
-        
-        againButton.innerHTML = "Nytt försök";
-        
-        againButton.setAttribute("disabled", "disabled");
-        
-        againButton.setAttribute("class", "notInUse");
-        
-        againButton.onclick = function ()
+        this.init = function ()
         {
-            div.removeChild(table);
-            div.removeChild(pressDiv);
-            div.removeChild(againButton);
-            This.init();
-        };
-        
-                    clickEvent = function (e)
-                    {
-                   
-                    e.preventDefault();
-                    
-                    var presentAlternative = e.currentTarget.getAttribute("id");
-                    
-                    if (lastAlternative === presentAlternative)
-                    {
+            var i = 0;
+            var x = 0;
+            var countPic = 0;
+            var picUp = 0;
+            var countGuesses = 0;
+            var duoFounded = 0;
+            var This = this;
+            var lastAlternativ = null;
+            var againButton = document.createElement("button");
+            var p = document.createElement("p");
+            var pressDiv = document.createElement("div");
+            var table = document.createElement("table");
+            var div = document.getElementById(memoryID);
+            var clickEvent;
+            var img;
+            var anchor;
+            var tr;
+            var td;
 
+            this.randomArray = RandomGenerator.getPictureArray(rows, cols);
+            
+            pressDiv.setAttribute("id", memoryID + "info");
+            
+            pressDiv.setAttribute("class", "Boardinfo");
+            
+            pressDiv.appendChild(p);
+
+            againButton.setAttribute("type", "button");
+            
+            againButton.innerHTML = "Nytt försök";
+            
+            againButton.setAttribute("disabled", "disabled");
+            
+            againButton.setAttribute("class", "notInUse");
+            
+            againButton.onclick = function ()
+            {
+                
+                div.removeChild(table);
+                
+                div.removeChild(pressDiv);
+                
+                div.removeChild(againButton);
+                
+                This.init();
+                
+            };
+            
+            clickEvent = function (e)
+            {
+                var currentChoice = e.currentTarget.getAttribute("id");
+                
+                e.preventDefault();
+
+                if (lastAlternativ === currentChoice)
+                {
+                } 
+                
+                else
+                {
+                    
+                    if (picUp < 2)
+                    {
+                        
+                        This.turnUp(currentChoice);
+                        
+                        picUp += 1;
+                        
+                        if (!lastAlternativ) 
+                        {
+                            
+                            lastAlternativ = currentChoice;
+                            
+                        }
                     }
                     
-                    else
+                    if (picUp >= 2)
                     {
-
-                        if (picUp < 2)
+                        
+                        if (This.compareSrc(currentChoice, lastAlternativ))
                         {
-
-                            This.turnUp(presentAlternative);
-
-                            picUp += 1;
+                            This.setEventToNull(currentChoice);
                             
-                            if (!lastAlternative)
-                            {
-                                
-                                lastAlternative = presentAlternative;
-                                
-                            }
-
+                            This.setEventToNull(lastAlternativ);
+                            
+                            picUp = 0;
+                            
+                            lastAlternativ = null;
+                            
+                            countGuesses += 1;
+                            
+                            duoFounded += 1;
+                            
+                            This.writeInfo("Antal par kvar att hitta: " + (This.randomArray.length / 2 - duoFounded) + "<br />Antal gjorda gissningar: " + countGuesses);
                         }
-
-                        if (picUp >= 2)
+                        
+                        else
                         {
-
-                            if (This.compareSrc(presentAlternative, lastAlternative))
+                            
+                            setTimeout(function ()
                             {
-
-                                This.setEventToNull(presentAlternative);
-
-                                This.setEventToNull(lastAlternative);
-
-                                picUp = 0;
-
-                                lastAlternative = null;
                                 
-                                duoFounded += 1;
+                                This.turnDown(currentChoice);
+                                
+                                This.turnDown(lastAlternativ);
+                                
+                                picUp = 0;
+                                
+                                lastAlternativ = null;
                                 
                                 countGuesses += 1;
                                 
-                                This.writeInfo("Par kvar att hitta: " + (This.memoryCards.length / 2 - duoFounded) + "<br />Antal gissningar kvar : " + countGuesses);
+                                This.writeInfo("Antal par kvar att hitta: " + (This.randomArray.length / 2 - duoFounded) + "<br />Antal gjorda gissningar: " + countGuesses);
                                 
-                            }
-                            
-                            else
-                            {
-                                    setTimeout(function ()
-                                    {
-                                        
-                                    This.turnDown(presentAlternative);
-
-                                    This.turnDown(lastAlternative);
-
-                                    picUp = 0;
-
-                                    lastAlternative = null;
-                                    
-                                    countGuesses += 1;
-                                    
-                                    This.writeInfo("Par kvar att hitta: " + (This.memoryCards.length / 2 - duoFounded) + "<br />Antal gissningar kvar : " + countGuesses);
-
-                                },
-                                1000);
-                            }
-
+                            }, 
+                            1000);
                         }
+                    }
+                    
+                    if (duoFounded >= This.randomArray.length / 2)
+                    {
                         
-                        if (duoFounded >= This.memoryCards.length / 2)
-                        
-                        {
-                            
                         againButton.removeAttribute("disabled");
                         
                         againButton.removeAttribute("class");
                         
-                        This.writeInfo("Du behövde " + countGuesses +
-                            " gissningar för att vinna!");
-                        }
-
+                        This.writeInfo("Du behövde " + countGuesses + " gissningar för att vinna!");
+                        
                     }
+                }
 
-                };
+            };
 
-        for (i = 1; i <= rows; i += 1)
-        {
-            
-           for (x = 1; x <= cols; x += 1)
-           {
-               
-               img.setAttribute("src", "pics/0.png");
+            for (i = 1; i <= rows; i += 1) 
+            {
+                
+                tr = document.createElement("tr");
+                
+                for (x = 1; x <= cols; x += 1)
+                {
+                    
+                    img = document.createElement("img");
+                    
+                    img.setAttribute("src", "pics/0.png");
+                    
+                    anchor = document.createElement("a");
+                    
+                    anchor.setAttribute("class", (this.randomArray[countPic] + " Klickbar") );
+                    
+                    anchor.setAttribute("id", countPic + memoryID);
+                    
+                    anchor.setAttribute("href", "#");
+                    
+                    td = document.createElement("td");
+                    
+                    anchor.appendChild(img);
+                    
+                    td.appendChild(anchor);
+                    
+                    tr.appendChild(td);
 
-               anchor.appendChild(img);
-
-               td.appendChild(anchor);
-
-               tr.appendChild(td);
-               
-               anchor.setAttribute("class", (this.randomArray[picCounter] + "Klickbar!"));
-
-               anchor.setAttribute("href", "#");
-               
-               anchor.setAttribute("id", picCounter + memoryID);
-               
-               picCounter += 1;
-               
-               anchor.onclick = clickEvent;
-
+                    countPic += 1;
+                    
+                    anchor.onclick = clickEvent;
+                    
+                }
+                
+                table.appendChild(tr);
+                
             }
 
-            table.appendChild(tr);
-
-        }
-
-        div.appendChild(table);
-        
-        div.appendChild(pressDiv);
-        
-        div.appendChild(againButton);
-        
-        This.writeInfo("Par kvar att hitta: " + (This.memoryCards.length / 2 - duoFounded) + "<br />Antal gissningar kvar : " + countGuesses);
-        
-    };
-    
-        this.turnDown = function (nodeId)
-        {
+            div.appendChild(table);
             
-        var node = document.getElementById(nodeId);
-
-        node.firstChild.setAttribute("src", "pics/0.png");
-
+            div.appendChild(pressDiv);
+            
+            div.appendChild(againButton);
+            
+            this.writeInfo("Antal par kvar : " + (this.randomArray.length / 2 - duoFounded) + "<br />Antal gissningar: " + countGuesses);
+            
         };
         
-        this.turnUp = function (nodeId)
+        this.setEventToNull = function (nodeId)
         {
             
-        var node = document.getElementById(nodeId),
-        
-        index = node.getAttribute("class");
+            var node = document.getElementById(nodeId);
             
-        node.firstChild.setAttribute("src", "pics/" + index + ".png");
-
-
+            node.setAttribute("class", (node.getAttribute("class").replace("Klickbar", "Oklickbar")));
+            
+            node.onclick = null;
+            
+        };
+        
+        this.writeInfo = function (text)
+        {
+            
+            var node = document.getElementById(memoryID + "info");
+            
+            node.firstChild.innerHTML = text;
+            
         };
         
         this.compareSrc = function (nodeId1, nodeId2)
+        
         {
-
-        var src1 = document.getElementById(nodeId1).firstChild.getAttribute("src");
-
-        var src2 = document.getElementById(nodeId2).firstChild.getAttribute("src");
-
-        return (src1 === src2);
-
+            var src1 = document.getElementById(nodeId1).firstChild.getAttribute("src");
+            
+            var src2 = document.getElementById(nodeId2).firstChild.getAttribute("src");
+            
+            return (src1 === src2);
+            
         };
 
-        this.setEventToNull = function (nodeId)
+        this.turnDown = function (nodeId)
         {
-
+            
             var node = document.getElementById(nodeId);
-
-           node.onclick = null;
-
+            
+            node.firstChild.setAttribute("src", "pics/0.png");
+            
         };
-
-        this.writeInfo = function (text) 
+        
+        this.turnUp = function (nodeId)
+        
         {
-
-        var node = document.getElementById(memoryID + "info");
-
-        node.firstChild.innerHTML = text;
-
+            var node = document.getElementById(nodeId);
+            
+            var index = parseInt(node.getAttribute("class"));
+            
+            node.firstChild.setAttribute("src", "pics/" + index + ".png");
+            
         };
+    }
+    
+    new MemoryBoard("Board", 6, 6).init();
     
 }
-    new MemoryBoard("Board", 3, 4).init();
-    
-}());
+());
