13132e4817be4c18b6b1f704c8a83de213d38d89
Kevin Fredriksson <kf222dh@student.lnu.se>
2014-01-10 18:06:36 -0500

    Klar med Ajax

---
+++ 5-pwd/script/AjaxCon.js
@@ -0,0 +1,53 @@
+"use strict"
+
+function AjaxCon(url, callback)
+{
+
+        var READY_STATE_UNINITIALIZED = 0;
+        var READY_STATE_OPENED = 1;
+        var READY_STATE_SENT = 2;
+        var READY_STATE_LOADING = 3;
+        var READY_STATE_COMPLETE = 4;
+        var xhr = new XMLHttpRequest();
+
+        xhr.onreadystatechange = function()
+        {
+                
+                if(xhr.readyState === READY_STATE_COMPLETE)
+                {
+                        if(xhr.status >= 200 && xhr.status < 300 || xhr.status === 304)
+                        {
+                                callback(xhr.responseText);                                
+                        }
+                        else
+                        {
+                                console.log("Läsfel"+xhr.status);        
+                        }
+                }
+        };
+
+        xhr.open("get", url, true);
+        //xhr.setRequestHeader('If-Modified-Since', 'Mon, 01 Sep 2007 00:00:00 GMT'); Funkar inte med
+        xhr.send(null);
+  }
+
+  AjaxCon.prototype.getXHR = function()
+  {
+                var xhr = null;
+                try 
+                {
+                        xhr = new XMLHttpRequest();        
+                } 
+                catch (error)
+                {
+                        try 
+                        {
+                                xhr = new ActiveXObject("Microsoft.XMLHTTP");        
+                        } 
+                        catch (error)
+                        {
+                                throw new Error("No XHR object available");
+                        }
+                }
+                return xhr;
+  };
